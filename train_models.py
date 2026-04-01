3ffe8250bf21bbb4a757495919087e6fff9369f2
Midnight_W4lker <aliabid.2259@gmail.com>
2026-04-01 19:49:41 +0500

    added genism

--- train_models.py
+++ train_models.py
@@ -16,7 +16,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import (accuracy_score, classification_report,
-                             confusion_matrix)
+                            confusion_matrix)
 from sklearn.model_selection import train_test_split
 from gensim.models import Word2Vec
 
